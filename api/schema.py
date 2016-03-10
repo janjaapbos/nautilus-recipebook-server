8d265a7e1a270a01d9e1344b1be032560bf10b32
Janjaap Bos <janjaap.bos@capitar.com>
2016-03-10 17:00:35 +0100

    AddRecipeMutation

--- api/schema.py
+++ api/schema.py
@@ -1,5 +1,7 @@
-from graphene import String, ObjectType, Schema
+from graphene import Schema, ObjectType, String, Mutation, Boolean, Field
 from nautilus.api import ServiceObjectType, Connection
+from nautilus.network import dispatch_action
+from nautilus.conventions import getCRUDAction
 from recipeBook.recipe import service as RecipeService
 from recipeBook.ingredient import service as IngredientService
 
@@ -21,5 +23,35 @@ class Query(ObjectType):
     recipes = Connection(Recipe)
 
 
+class AddRecipeMutation(Mutation):
+    """
+        This mutation fires an event to create a new recipe in the model service.
+    """
+    class Input:
+        """
+            This class defines the mutation arguments.
+        """
+        name = String()
+
+
+    success = Boolean(description="Whether or not the dispatch was successful")
+
+    @classmethod
+    def mutate(cls, instance, args, info):
+        """ perform the mutation """
+        # send the new recipe action into the queue
+        payload = dict(name=args['name'])
+        dispatch_action(
+            action_type=getCRUDAction('create', RecipeService.model),
+            payload=payload
+        )
+
+
+class ApiMutations(ObjectType):
+    """ the list of mutations that the api supports """
+    addRecipe = Field(AddRecipeMutation)
+
+
 schema = Schema()
 schema.query = Query
+schema.mutation = ApiMutations
